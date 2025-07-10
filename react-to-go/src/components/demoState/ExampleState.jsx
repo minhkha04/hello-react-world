6fba893fc0ffc4a0d17cf1726d05b689cb7b441b
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-07-10 12:47:12 +0700

    style demo react state

--- react-to-go/src/components/demoState/ExampleState.jsx
+++ react-to-go/src/components/demoState/ExampleState.jsx
@@ -32,25 +32,27 @@ const ExampleState = () => {
     ];
     const [doctor, setDoctor] = useState("https://res.cloudinary.com/di6hi1r0g/image/upload/v1751524777/uploads/avt_08e1c237-20ee-4c3f-9fdf-00d5e6d0d06c.png");
     return (
-        <div className="">
-            <img className="h-80 object-cover rounded-md" src={doctor} alt=""/>
-            {listDoctors.map((doc) => {
-                const isSelected = doctor === doc.avatarUrl;
-                return (
-                    <button
-                        className={`py-2 px-5 border-2 rounded-md me-3 transition-colors duration-200 ${
-                            isSelected
-                                ? 'border-green-500 bg-green-500 text-white'
-                                : 'border-purple-400 bg-purple-400 text-white hover:bg-white hover:text-purple-400 hover:border-purple-400 active:bg-purple-800'
-                        }`}
-                        onClick={() => {
-                            setDoctor(doc.avatarUrl)
-                        }}
-                    >
-                        {doc.fullName}
-                    </button>
-                )
-            })}
+        <div className="grid grid-cols-2 w-1/2">
+            <img className="h-80 object-cover rounded-md col-span-1" src={doctor} alt=""/>
+            <div className="w-64 flex flex-col col-span-1">
+                {listDoctors.map((doc) => {
+                    const isSelected = doctor === doc.avatarUrl;
+                    return (
+                        <button
+                            className={`py-2 px-5 border-2 rounded-md mt-3 transition-colors duration-200 ${
+                                isSelected
+                                    ? 'border-green-500 bg-green-500 text-white'
+                                    : 'border-purple-400 bg-purple-400 text-white hover:bg-white hover:text-purple-400 hover:border-purple-400 active:bg-purple-800'
+                            }`}
+                            onClick={() => {
+                                setDoctor(doc.avatarUrl)
+                            }}
+                        >
+                            {doc.fullName}
+                        </button>
+                    )
+                })}
+            </div>
         </div>
     );
 };
